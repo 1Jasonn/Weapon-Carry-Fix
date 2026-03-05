c6fc32d1ef5baac44603fb1123b1a85f8f65ad62
1Jasonn <Jasonween2014@outlook.com>
2026-03-05 18:17:31 +0000

    ss

---
+++ weapons-carry/config.lua
@@ -0,0 +1,263 @@
+Config = Config or {}
+
+Config.SlingCompatibleWeapons = {
+
+    [`weapon_sniperrifle`] = {
+        itemName = 'weapon_sniperrifle',
+        model = 'w_sr_sniperrifle',
+        bone = 24816,
+        x = 0.075,
+        y = -0.15,
+        z = -0.15,
+        rotX = 90.0,
+        rotY = 0.0,
+        rotZ = 0.0
+    },
+
+    [`WEAPON_SIG716`] = {
+        itemName = 'weapon_sig716',
+        model = 'mdx_sig716',
+        bone = 24816,
+        x = 0.075,
+        y = -0.15,
+        z = -0.15,
+        rotX = 90.0,
+        rotY = 0.0,
+        rotZ = 0.0
+    },
+
+    [`WEAPON_SIGMCX`] = {
+        itemName = 'weapon_sigmcx',
+        model = 'w_ar_sigmcx',
+        bone = 24816,
+        x = 0.10,
+        y = 0.27,
+        z = 0.02,
+        rotX = 0.0,
+        rotY = -30.0,
+        rotZ = 175.0
+    },
+
+    [`weapon_bat`] = {
+        itemName = 'weapon_bat',
+        model = 'w_me_bat',
+        bone = 24818,
+        x = -0.25,
+        y = -0.17,
+        z = -0.05,
+        rotX = 0.10,
+        rotY = -70.0,
+        rotZ = 180.0
+    },
+
+    [`weapon_batmetal`] = {
+        itemName = 'weapon_batmetal',
+        model = 'w_me_bat',
+        bone = 24818,
+        x = -0.25,
+        y = -0.17,
+        z = -0.05,
+        rotX = 0.10,
+        rotY = -70.0,
+        rotZ = 180.0
+    },
+
+    [`weapon_poolcue`] = {
+        itemName = 'weapon_poolcue',
+        model = 'w_me_poolcue',
+        bone = 24818,
+        x = -0.25,
+        y = -0.16,
+        z = -0.06,
+        rotX = 0.10,
+        rotY = -70.0,
+        rotZ = 180.0
+    },
+
+    [`weapon_golfclub`] = {
+        itemName = 'weapon_golfclub',
+        model = 'w_me_gclub',
+        bone = 24818,
+        x = -0.25,
+        y = -0.17,
+        z = -0.05,
+        rotX = 0.10,
+        rotY = -70.0,
+        rotZ = 180.0
+    },
+
+    [`weapon_wrench`] = {
+        itemName = 'weapon_wrench',
+        model = 'w_me_wrench',
+        bone = 24818,
+        x = -0.15,
+        y = -0.17,
+        z = -0.00,
+        rotX = 0.10,
+        rotY = -70.0,
+        rotZ = 180.0
+    },
+
+    [`weapon_zk`] = {
+        itemName = 'weapon_zk',
+        model = 'w_me_zombieknife_01',
+        bone = 24818,
+        x = -0.25,
+        y = -0.17,
+        z = -0.05,
+        rotX = 0.10,
+        rotY = -70.0,
+        rotZ = 180.0
+    },
+
+    [`weapon_airsoft870`] = {
+        itemName = 'weapon_airsoft870',
+        model = 'w_sg_pumpshotgun',
+        bone = 24818,
+        x = -0.066,
+        y = -0.199,
+        z = 0.199,
+        rotX = 0.00,
+        rotY = -51.82,
+        rotZ = 180.0
+    },
+
+    [`weapon_c36`] = {
+        itemName = 'weapon_c36',
+        model = 'w_ar_carbinerifle',
+        bone = 24818,
+        x = 0.08,
+        y = 0.20,
+        z = -0.02,
+        rotX = 10.0,
+        rotY = 135.0,
+        rotZ = 0.2
+    },
+
+    [`weapon_fm1_honeybadger`] = {
+        itemName = 'weapon_fm1_honeybadger',
+        model = 'w_ar_carbinerifle',
+        bone = 24816,
+        x = 0.2,
+        y = 0.22,
+        z = 0.035,
+        rotX = 0.0,
+        rotY = 320.0,
+        rotZ = 175.0
+    },
+
+    [`weapon_mp5`] = {
+        itemName = 'weapon_mp5',
+        model = 'w_sb_mp5',
+        bone = 24816,
+        x = 0.2,
+        y = 0.22,
+        z = 0.035,
+        rotX = 0.0,
+        rotY = 320.0,
+        rotZ = 175.0
+    },
+
+    [`weapon_combatrifle`] = {
+        itemName = 'weapon_combatrifle',
+        model = 'w_ar_assaultrifle',
+        bone = 24818,
+        x = -0.0,
+        y = -0.15,
+        z = -0.10,
+        rotX = 0.10,
+        rotY = 180.0,
+        rotZ = 0.0
+    },
+
+    [`weapon_l104a1`] = {
+        itemName = 'weapon_l104a1',
+        model = 'w_ar_specialcarbine',
+        bone = 24818,
+        x = -0.0,
+        y = -0.15,
+        z = -0.10,
+        rotX = 0.10,
+        rotY = 180.0,
+        rotZ = 0.0
+    },
+
+    [`weapon_blackgangbat`] = {
+        itemName = 'weapon_blackgangbat',
+        model = 'w_me_bat',
+        bone = 24816,
+        x = 0.035,
+        y = -0.147,
+        z = -0.08,
+        rotX = 180.0,
+        rotY = 90.0,
+        rotZ = -4.0
+    },
+
+    [`weapon_bluegangbat`] = {
+        itemName = 'weapon_bluegangbat',
+        model = 'w_me_bat',
+        bone = 24816,
+        x = 0.035,
+        y = -0.147,
+        z = -0.08,
+        rotX = 180.0,
+        rotY = 90.0,
+        rotZ = -4.0
+    },
+
+    [`weapon_redgangbat`] = {
+        itemName = 'weapon_redgangbat',
+        model = 'w_me_bat',
+        bone = 24816,
+        x = 0.035,
+        y = -0.147,
+        z = -0.08,
+        rotX = 180.0,
+        rotY = 90.0,
+        rotZ = -4.0
+    },
+
+    [`weapon_hatchet`] = {
+        itemName = 'weapon_hatchet',
+        model = 'w_me_hatchet',
+        bone = 24816,
+        x = 0.035,
+        y = -0.147,
+        z = -0.08,
+        rotX = 180.0,
+        rotY = 90.0,
+        rotZ = -4.0
+    },
+
+    [`weapon_battleaxe`] = {
+        itemName = 'weapon_battleaxe',
+        model = 'w_me_battleaxe',
+        bone = 24816,
+        x = 0.035,
+        y = -0.147,
+        z = -0.08,
+        rotX = 180.0,
+        rotY = 90.0,
+        rotZ = -4.0
+    },
+    [`weapon_stone_hatchet`] = {
+        itemName = 'weapon_stone_hatchet',
+        model = 'w_me_stonehatchet',
+        bone = 24816,
+        x = 0.035,
+        y = -0.147,
+        z = -0.08,
+        rotX = 180.0,
+        rotY = 90.0,
+        rotZ = -4.0
+    }
+}
+
+Config.SlingWeaponComponents = {
+    ['WEAPON_SIGMCX'] = {
+        ['at_flashlight'] = `sig_flsh`,
+        ['at_scope_medium'] = `w_at_sigmcxscope`,
+        ['at_clip_extended_rifle'] = `w_ar_sigmcx_mag1`,
+    },
+}
